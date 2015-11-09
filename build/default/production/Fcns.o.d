a15dfa6fc8014d16503a01e91a3aa60e6c97a66a
Michael Deng <mmdeng17@gmail.com>
2015-11-09 16:27:08 -0500

    misc build stuff

--- build/default/production/Fcns.o.d
+++ build/default/production/Fcns.o.d
@@ -1,3 +1,5 @@
 build/default/production/Fcns.o:  \
 	C:/Users/Public/Documents/ECGGating.X/Fcns.c  \
+	C:/Users/Public/Documents/ECGGating.X/Lcd.h  \
+	C:/Users/Public/Documents/ECGGating.X/General.h  \
 
